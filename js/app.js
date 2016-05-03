d1bd76391365feb59715dc45eb7b2f49cd290791
JONATHANJAGER <jjagercareer@gmail.com>
2016-05-03 19:51:13 -0400

    added to hadouken

--- js/app.js
+++ js/app.js
@@ -12,7 +12,13 @@ $(document).ready(function() {
     // play hadouken sound
     $('.ryu-ready').hide();
   $('.ryu-throwing').show();
-  $('.hadouken').show();
+  $('.hadouken').show().animate(
+    {'letf': '1020px'}, 500,
+    function(){
+      $(this).hide();
+      $(this).css('left','520px');
+    }
+    );
     // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
